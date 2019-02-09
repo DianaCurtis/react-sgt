4215a2c8df09630c6cba3f46878912c15e5308e5
Diana Curtis <dianacurtis92@gmail.com>
2019-02-08 18:10:57 -0800

    add the student form

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,26 @@
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
 import '../assets/css/app.scss';
 
 
 import React from 'react';
+import AddStudent from './add_student';
 import Table from './table';
+
 
 
 
 const App = () => (
     <div>
        <h1 className="center">SGT</h1>
-        <Table/>
+        <div className="row">
+            <div className="col s12 m8">
+                <Table></Table>
+            </div>
+            <div className="col s12 m4">
+                <AddStudent/>
+            </div>
+        </div>
     </div>
 );
 
